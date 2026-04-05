41f1ea4975929678c207fdfe64048eb7bebc015f
Shrish Singh Sourya <shrishss007@gmail.com>
2026-04-05 18:01:48 +0530

    blog page heading fix

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -20,7 +20,7 @@ export default async function BlogsPage() {
   return (
     <section className="section-container page-section">
       <Badge>Editorial</Badge>
-      <h1 className="page-title mt-4">Stories and Essays</h1>
+      <h1 className="page-title mt-4">Blog</h1>
       <div className="mt-10 grid gap-6 md:grid-cols-2 lg:grid-cols-3">
         {blogs.map((blog) => (
           <Link href={`/blogs/${blog.slug}`} key={blog.id}>
